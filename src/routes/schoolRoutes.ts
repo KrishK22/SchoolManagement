d0a4e085fb1d1e6cd8f79abee39ab85f1eb5cb5d
Krish Kumar <krish.k2024@gmail.com>
2025-04-29 22:58:23 +0530

    listSchools

--- src/routes/schoolRoutes.ts
+++ src/routes/schoolRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express'
-import { addSchool } from '../controllers/schoolControllers'
+import { addSchool, getSchools } from '../controllers/schoolControllers'
 const router = express.Router()
 
 
 router.post('/addSchool', addSchool)
+
+router.get('/listSchools', getSchools)
 
 
 export default router
