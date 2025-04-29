e27bceed7a7ae7f2fd1bde060b4e6d3b762ac829
Krish Kumar <krish.k2024@gmail.com>
2025-04-29 23:35:42 +0530

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import schoolRoutes from './routes/schoolRoutes'
 dotenv.config()
 const PORT = process.env.PORT
@@ -8,6 +9,12 @@ const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 
-app.use('/school', schoolRoutes)
+app.use(cors({
+    origin: '*',
+    methods: ['POST', 'GET']
+}))
+
+
+app.use('/api', schoolRoutes)
 
 app.listen(PORT)
